a34ccaf57b45e2dd0b2a9d11f680486a796e3b3f
Marco Souza <Marco Souza>
2017-04-15 18:48:34 -0300

    Estrutura básica e início de desenvolvimento da primeira tela

---
+++ js/creative.js
@@ -0,0 +1,26 @@
+(function($) {
+    "use strict"; // start of use strict
+
+    $('.banner-buttons ul li').on('click', function(){
+        // get the actual banner on the page
+        var currentImage = $(this).attr('data-banner');
+        // set default path to images
+        var path = 'img/banners/';
+        // transiction effect when change the image
+        $('#banner').fadeOut(function(){
+            $(this).attr('src', path + currentImage + '.jpg');
+            $(this).fadeIn();
+        });
+        // remove the style of current active item
+        removeActiveItens();
+        // add the active style for the element selected
+        $(this).addClass('active');
+    });
+
+    function removeActiveItens() {
+        $('.banner-buttons ul li').each(function(){
+            $(this).removeClass('active');
+        });
+    }
+
+})(jQuery); // end of use strict
